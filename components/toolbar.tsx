4963baec89929a023971dd129373c217e6b2ffa9
Jorgemayor <jorge.e.mayor@gmail.com>
2022-11-28 08:13:14 +0300

    style: run prettier

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,20 +1,24 @@
-import styles from '../styles/Toolbar.module.css'
-import Image from 'next/image'
-import {Dispatch, FC, SetStateAction} from "react";
+import styles from "../styles/Toolbar.module.css"
+import Image from "next/image"
+import { Dispatch, FC, SetStateAction } from "react"
 
 interface Props {
-    setLanguageId: Dispatch<SetStateAction<number>>,
+    setLanguageId: Dispatch<SetStateAction<number>>
 }
 
-export const Toolbar: FC<Props> = ({setLanguageId}) => {
-
+export const Toolbar: FC<Props> = ({ setLanguageId }) => {
     const manageClick = () => {
         setLanguageId(2)
     }
 
     return (
         <div className={styles.main}>
-            <Image src={"/logo.svg"} alt={"ITMO Logo"} width={161.33} height={16}/>
+            <Image
+                src={"/logo.svg"}
+                alt={"ITMO Logo"}
+                width={161.33}
+                height={16}
+            />
             <button onClick={manageClick}></button>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,53 @@
-import styles from '../styles/Home.module.css'
-import {Toolbar} from "../components/toolbar";
-import {useState} from "react";
-import Head from "next/head";
+import styles from "../styles/Home.module.css"
+import { Toolbar } from "../components/toolbar"
+import { useState } from "react"
+import Head from "next/head"
 
 type New = {
-    image_big: string,
-    date: string,
-    lead: string,
+    image_big: string
+    date: string
+    lead: string
 }
 
 interface Props {
     news: Array<New>
 }
 
-export const Home = ({news}: Props) => {
-
+export const Home = ({ news }: Props) => {
     const [languageId, setLanguageId] = useState<number>(1)
 
-  return (
-      <div className='page-container'>
-          <Head>
-              <title>ITMO news</title>
-              <meta name="description" content="Web app to read news of ITMO"/>
-              <link rel="icon" href="/favicon.ico"/>
-          </Head>
-          <Toolbar setLanguageId={setLanguageId}/>
+    return (
+        <div className="page-container">
+            <Head>
+                <title>ITMO news</title>
+                <meta
+                    name="description"
+                    content="Web app to read news of ITMO"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Toolbar setLanguageId={setLanguageId} />
 
-          <div className={styles.main}>
-              <h1>Новости и события</h1>
-              <div>
-                  {
-                      news ? (news.map((article: New, key: number) =>
-                          <h2 key={key}>{article.date}</h2>
-                      )) : <></>
-                  }
-              </div>
-          </div>
-      </div>
-  )
+            <div className={styles.main}>
+                <h1>Новости и события</h1>
+                <div>
+                    {news ? (
+                        news.map((article: New, key: number) => (
+                            <h2 key={key}>{article.date}</h2>
+                        ))
+                    ) : (
+                        <></>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export const getServerSideProps = async () => {
-
     const apiResponse = await fetch(
         `https://news.itmo.ru/api/news/list/?ver=2.0&lead=2&per_page=2&language_id=1`
-    ).then(res => res.json())
+    ).then((res) => res.json())
 
     const { news } = apiResponse
 
