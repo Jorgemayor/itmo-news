3a3fe229376405d354ec76bfe71e9746cbbb422b
Jorgemayor <jorge.e.mayor@gmail.com>
2022-11-29 04:00:51 +0300

    feat: create template article

---
+++ pages/article.tsx
@@ -0,0 +1,12 @@
+import { Toolbar } from "../components/toolbar"
+import styles from "../styles/Article.module.css"
+
+export const Article = () => {
+    return (
+        <div className={styles.main}>
+            <Toolbar />
+        </div>
+    )
+}
+
+export default Article
