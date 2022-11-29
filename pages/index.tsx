db03b9ec22e006b7471dabe4a2427f5ff64331c2
Jorgemayor <jorge.e.mayor@gmail.com>
2022-11-29 03:59:52 +0300

    feat: add provisional link, change icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,35 @@
 import styles from "../styles/Home.module.css"
 import { Toolbar } from "../components/toolbar"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Head from "next/head"
 
 type New = {
+    title: string
     image_big: string
     date: string
     lead: string
+    url: string
 }
 
-interface Props {
-    news: Array<New>
-}
-
-export const Home = ({ news }: Props) => {
+export const Home = () => {
     const [languageId, setLanguageId] = useState<number>(1)
+    const [news, setNews] = useState<New[]>()
+
+    const getServerSideProps = async () =>
+        await fetch(
+            `https://news.itmo.ru/api/news/list/?ver=2.0&lead=2&per_page=9&language_id=${languageId}`
+        )
+            .then((res: Response) => res.json())
+            .catch((reason: any) => {
+                console.log(reason)
+            })
+
+    useEffect(() => {
+        getServerSideProps().then((data) => {
+            const { news } = data
+            setNews(news)
+        })
+    }, [languageId])
 
     return (
         <div className="page-container">
@@ -24,16 +39,32 @@ export const Home = ({ news }: Props) => {
                     name="description"
                     content="Web app to read news of ITMO"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/itmo_icon.png" />
             </Head>
             <Toolbar setLanguageId={setLanguageId} />
 
             <div className={styles.main}>
-                <h1>Новости и события</h1>
-                <div>
+                <div className={styles.title}>
+                    <h1>Новости и события</h1>
+                </div>
+                <div className={styles.news}>
                     {news ? (
-                        news.map((article: New, key: number) => (
-                            <h2 key={key}>{article.date}</h2>
+                        news.map((article: New, index: number) => (
+                            <div
+                                onClick={() =>
+                                    (window.location.href = article.url)
+                                }
+                                className={styles.article}
+                                key={index}
+                            >
+                                <img
+                                    src={article.image_big}
+                                    alt={article.title}
+                                />
+                                <h5>{article.date}</h5>
+                                <p>{article.title}</p>
+                                {/*<div dangerouslySetInnerHTML={{ __html: article.lead }}></div>*/}
+                            </div>
                         ))
                     ) : (
                         <></>
@@ -42,20 +73,6 @@ export const Home = ({ news }: Props) => {
             </div>
         </div>
     )
-}
-
-export const getServerSideProps = async () => {
-    const apiResponse = await fetch(
-        `https://news.itmo.ru/api/news/list/?ver=2.0&lead=2&per_page=2&language_id=1`
-    ).then((res) => res.json())
-
-    const { news } = apiResponse
-
-    return {
-        props: {
-            news,
-        },
-    }
 }
 
 export default Home
